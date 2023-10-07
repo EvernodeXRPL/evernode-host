55bc60bf629e5510899399d088253ee7dcdd7216
ishwo <ishwolab@gmail.com>
2023-10-07 13:55:02 -0400

    add ALB, DNS Record, ACM

--- evernodehost/bin/evernodehost.ts
+++ evernodehost/bin/evernodehost.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EvernodeHostStack } from '../lib/evernode-host-stack';
-// import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 // dotenv.config();
 
 
@@ -16,7 +16,7 @@ const AWS_ACCOUNT = process.env.AWS_ACCOUNT
 new EvernodeHostStack(app, 'EvernodeHostStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- evernodehost/lib/evernode-host-stack.ts
+++ evernodehost/lib/evernode-host-stack.ts
@@ -4,6 +4,9 @@ import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'; 
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'; 
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { PublicHostedZone, HostedZone, ARecord, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { LoadBalancerTarget} from 'aws-cdk-lib/aws-route53-targets';
+import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager';
 
 
 export class EvernodeHostStack extends cdk.Stack {
@@ -29,30 +32,64 @@ export class EvernodeHostStack extends cdk.Stack {
       }
     );
 
-    // Set up the AWS Application LoadBalancer to loadblance inbound traffic to backend Evernode Host Service
-    const lb = new elbv2.ApplicationLoadBalancer(this, 'HotpotLB', {
+    // Now let's set up dns record with existing domain name and integrate with load balancer 
+    // Get the existing hosted zone
+    const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'EvernodeHostHostedZone', {
+      hostedZoneId: 'YOUR_HOSTED_ZONE_ID',
+      zoneName: 'example.com',
+    });
+    // Request an ACM certificate
+    const certValidation = new certificatemanager.Certificate(this, 'EvernodeHostCertificate', {
+      domainName: 'hello.example.com',
+      certificateName: 'EvernodeHostCert', // Optionally provide an certificate name
+      validation: certificatemanager.CertificateValidation.fromDns(hostedZone),
+    });
+
+
+    // Set up the firewall rules for Load Balancer 
+    // Create a security group
+    const securityGroup = new ec2.SecurityGroup(this, 'EvernodeHostSecurityGroup', {
+      vpc: vpc,
+    });
+
+    // Add an ingress rule to allow port 443 from everywhere
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow port 443 from everywhere');
+
+    // Set up the AWS Application LoadBalancer to loadblance and handle incoming traffics to backend Evernode Host Service
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'EvernodeHostLB', {
       vpc,
       internetFacing: true,
+      loadBalancerName: "EvernodeHostLB", 
+      securityGroup: securityGroup,
     });
-  
+
     // Create a LB listener with port 443 to handle inbound connection
     const listener = lb.addListener('Listener', {
       port: 443,
       open: true,
       certificates: [
-        elbv2.ListenerCertificate.fromArn('arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012')
+        elbv2.ListenerCertificate.fromArn(certValidation.certificateArn)
       ],
     });
   
     // Adding Target to backend Evernode Instnace
-    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'HotpotTargetGroup', {
-      vpc: new ec2.Vpc(this, 'Vpc'),
+    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'EvernodeHostTargetGroup', {
+      vpc: vpc,
       port: 80,
+      targetType: elbv2.TargetType.INSTANCE
     });
     
-    listener.addTargetGroups('HotpotTargetGroup', {
+    listener.addTargetGroups('EvernodeHostTargetGroup', {
       targetGroups: [targetGroup],
+      
     });
 
+    // Set up DNS record that puts the ELB created above 
+    new ARecord(this, 'ARecord', {
+      zone: hostedZone,
+      recordName: 'www.example.com',
+      target: RecordTarget.fromAlias(new LoadBalancerTarget(lb)),
+    });
+  
   }
 }
